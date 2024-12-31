4b847dd3f72146333d6308b79b57defa2cb3d795
Himank <k.himank.14@gmail.com>
2024-12-31 19:07:33 +0530

    added preprocessing to streamlit app

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import streamlit as st
+import re
 import pickle
 import numpy as np
-import pickle
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
+lemmatizer = WordNetLemmatizer()
 
-st.set_page_config(page_title="Sentiment Analysis", layout='centered')
-
-st.title('Word2Vector Sentiment Analysis App')
-st.write("Trained on kindle review dataset with a test accuracy of 77%.")
 
 with open('artifacts/classifier.pkl', 'rb+') as file:
     classifier = pickle.load(file)
@@ -24,6 +23,19 @@ def vectorize(column):
     for i in range(len(col_vec)):
         col_vec[i] = avg_word2vec(col_vec[i], wv_model)
     return np.array(col_vec)
+
+def preproecess_text(text):
+    text = re.sub('[^a-z A-z 0-9-]+', '', text)
+    text = ' '.join([lemmatizer.lemmatize(word, 'n') for word in text.split()])
+    text = ' '.join([lemmatizer.lemmatize(word, 'v') for word in text.split()])
+    text = ' '.join([lemmatizer.lemmatize(word, 'a') for word in text.split()])
+    text = ' '.join([lemmatizer.lemmatize(word, 'r') for word in text.split()])
+    return ' '.join([lemmatizer.lemmatize(word, 's') for word in text.split()])
+
+st.set_page_config(page_title="Sentiment Analysis", layout='centered')
+
+st.title('Word2Vector Sentiment Analysis App')
+st.write("Trained on kindle review dataset with a test accuracy of 77%.")
 
 text_input = st.text_area("Enter Review Text", height=150)
 
